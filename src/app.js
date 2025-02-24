d8eaa872a1db1a83c91bbcd31dd0bc0ecaf50e74
Shawon <shawonh986@gmail.com>
2025-02-25 01:13:16 +0800

    new product add api

--- src/app.js
+++ src/app.js
@@ -2,19 +2,20 @@ const express = require('express');
 const app = express();
 
 const port = 8000;
+app.use(express.json());
 
 const products = [
     {
-        name: "Sony",
-        model: "Xperia 1 vi",
-        color: "Off white",
-        price: "5600 rmb",
+        "name": "Sony",
+        "model": "Xperia 1 vi",
+        "color": "Off white",
+        "price": "5600 rmb",
     },
     {
-        name: "Iphone",
-        model: "16 pro max",
-        color: "Purple",
-        price: "9000 rmb",
+        "name": "Iphone",
+        "model": "16 pro max",
+        "color": "Purple",
+        "price": "9000 rmb",
     },
 ];
 
@@ -24,7 +25,12 @@ app.get('/status', (req,res)=>{
 
 app.get('/api/products', (req,res)=>{
     res.send(products);
-})
+});
+
+app.post('/api/products', (req,res)=>{
+    console.log(req.body);
+    res.send("Product added successfully");
+});
 
 app.listen(port, ()=>{
     console.log(`I am running on port ${port}`);
