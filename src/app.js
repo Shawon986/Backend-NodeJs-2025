d84ecf9736666dc88f4dd93d4770aa618cc1c391
Shawon <shawonh986@gmail.com>
2025-02-25 00:16:10 +0800

    create product details as object

--- src/app.js
+++ src/app.js
@@ -3,9 +3,25 @@ const app = express();
 
 const port = 8000;
 
+const products = [
+    {
+    name: "Sony",
+    model: "Xperia 1 vi",
+    color: "Off white",
+    price: "5600 rmb",
+},
+    {
+        name: "Iphone",
+        model: "16 pro max",
+        color: "Purple",
+        price: "9000 rmb",
+    }
+];
+
 app.get('/', (req,res)=>{
     res.send("OK")
 });
+
 
 app.listen(port, ()=>{
     console.log(`I am running on port ${port}`);
