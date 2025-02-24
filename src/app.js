70f0b108458a2dd0ae491799e6086e67882f6824
Shawon <shawonh986@gmail.com>
2025-02-25 00:46:46 +0800

    make get products api

--- src/app.js
+++ src/app.js
@@ -5,23 +5,26 @@ const port = 8000;
 
 const products = [
     {
-    name: "Sony",
-    model: "Xperia 1 vi",
-    color: "Off white",
-    price: "5600 rmb",
-},
+        name: "Sony",
+        model: "Xperia 1 vi",
+        color: "Off white",
+        price: "5600 rmb",
+    },
     {
         name: "Iphone",
         model: "16 pro max",
         color: "Purple",
         price: "9000 rmb",
-    }
+    },
 ];
 
-app.get('/', (req,res)=>{
+app.get('/status', (req,res)=>{
     res.send("OK")
 });
 
+app.get('/api/products', (req,res)=>{
+    res.send(products);
+})
 
 app.listen(port, ()=>{
     console.log(`I am running on port ${port}`);
