ec0d33b0106aafe6387cb43efbe75b35c98654f7
Shawon <shawonh986@gmail.com>
2025-02-25 02:32:08 +0800

    update product api

--- src/app.js
+++ src/app.js
@@ -7,14 +7,14 @@ app.use(express.json());
 
 const products = [
     {   
-        "_id": uuidv4(),
+        "_id": "944459b3-7413-4b6a-ad1b-24285b5eecbe",
         "name": "Sony",
         "model": "Xperia 1 vi",
         "color": "Off white",
         "price": "5600 rmb",
     },
     {   
-        "_id": uuidv4(),
+        "_id": "21b2834a-bd3a-4652-88f0-439a7c2b5636",
         "name": "Iphone",
         "model": "16 pro max",
         "color": "Purple",
@@ -37,6 +37,14 @@ app.post('/api/products', (req,res)=>{
     res.status(201).json(newProduct);
 });
 
+app.put('/api/products/:id', (req,res)=>{
+    const {id} = req.params;
+    const updatedProduct = products.find((products)=> products._id == id);
+    if(!updatedProduct){
+        return res.status(400).json({message:`No product found by id ${id}`})
+    }
+    res.json({message:"Product updated successfully"});
+})
 
 app.listen(port, ()=>{
     console.log(`I am running on port ${port}`);
