18e0210ab5e27ceed5681923aa7c22c4d2fa6b98
Shawon <shawonh986@gmail.com>
2025-02-28 00:42:22 +0800

    product service layer separated

--- src/router/productsRouter.js
+++ src/router/productsRouter.js
@@ -1,28 +1,14 @@
 const express = require('express');
 const productsRouter = express.Router();
 const { v4: uuidv4 } = require('uuid');
+const { productService } = require('./service/productService');
 
-const products = [
-    {   
-        "_id": "944459b3-7413-4b6a-ad1b-24285b5eecbe",
-        "name": "Sony",
-        "model": "Xperia 1 vi",
-        "color": "Off white",
-        "price": "5600 rmb",
-    },
-    {   
-        "_id": "21b2834a-bd3a-4652-88f0-439a7c2b5636",
-        "name": "Iphone",
-        "model": "16 pro max",
-        "color": "Purple",
-        "price": "9000 rmb",
-    },
-];
+
 
 
 
 productsRouter.get('/api/products', (req,res)=>{
-    res.send(products);
+    res.send(productService.getAllProducts());
 });
 
 
---
+++ src/router/service/productService.js
@@ -0,0 +1,25 @@
+
+
+const products = [
+    {   
+        "_id": "944459b3-7413-4b6a-ad1b-24285b5eecbe",
+        "name": "Sony",
+        "model": "Xperia 1 vi",
+        "color": "Off white",
+        "price": "5600 rmb",
+    },
+    {   
+        "_id": "21b2834a-bd3a-4652-88f0-439a7c2b5636",
+        "name": "Iphone",
+        "model": "16 pro max",
+        "color": "Purple",
+        "price": "9000 rmb",
+    },
+];
+
+const getAllProducts = ()=> products;
+
+
+module.exports = {
+    getAllProducts,
+}
