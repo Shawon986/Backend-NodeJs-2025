e74e37f601cbecb798e98d9d9814560a4478b751
Shawon <shawonh986@gmail.com>
2025-03-03 01:45:36 +0800

    configure router

--- CRUD-EXPRESS-JS/src/app.js
+++ CRUD-EXPRESS-JS/src/app.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-const {userRouter} = require('./router');
+const {configureRouter} = require('./router');
 app.use(express.json());
 
 
-//Check connection API
-app.get('/test', (req,res)=>{
-    res.send("HI THERE");
-});
-
-app.use('/api/users', userRouter);
+configureRouter(app);
 
 
 app.listen(port,()=>{
--- CRUD-EXPRESS-JS/src/router/index.js
+++ CRUD-EXPRESS-JS/src/router/index.js
@@ -1,5 +1,17 @@
 const userRouter = require('./userRouter');
 
+
+const configureRouter = (app)=>{
+
+    //Check connection API
+    app.get('/test', (req,res)=>{
+    res.send("HI THERE");
+    });
+
+    app.use('/api/users', userRouter);
+}
+
 module.exports = {
-    userRouter,
+
+    configureRouter,
 }
