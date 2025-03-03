b75a494111763737a3481bd29ceaab75a57a8dca
Shawon <shawonh986@gmail.com>
2025-03-03 22:32:40 +0800

    delete product api done

--- CRUD-EXPRESS-JS/src/router/productRouter.js
+++ CRUD-EXPRESS-JS/src/router/productRouter.js
@@ -13,6 +13,20 @@ productRouter.post('/',(req,res)=>{
     res.status(201).json(productServices.createNewProduct(req.body));
 });
 
+//Update product API
+productRouter.put('/:id',(req,res)=>{
+    const {id} = req.params;
+    const updatedProduct = productServices.updateProduct(id,req.body);
+    res.status(201).json(updatedProduct);
+});
+
+//Delete product API
+productRouter.delete('/:id',(req,res)=>{
+    const {id} = req.params;
+    productServices.deleteProduct(id);
+    res.json({message: "Product deleted successfully"})
+});
+
 
 
 
--- CRUD-EXPRESS-JS/src/services/productServices.js
+++ CRUD-EXPRESS-JS/src/services/productServices.js
@@ -3,13 +3,13 @@ const { v4: uuidv4 } = require('uuid');
 //Products data
 const products = [
     {
-        id:"111",
+        _id:"111",
         name: "16 pro max",
         brand: "iphone",
         price: "8500 rmb",
     },
     {
-        id:"222",
+        _id:"222",
         name: "14 pro max",
         brand: "iphone",
         price: "5500 rmb",
@@ -19,12 +19,32 @@ const products = [
 const getAllProducts = ()=> products;
 
 const createNewProduct =(productData)=>{
-    const newProduct = {id:uuidv4(), ...productData};
+    const newProduct = {_id:uuidv4(), ...productData};
     products.push(newProduct);
     return newProduct;
+};
+
+const updateProduct = (id,product)=>{
+    let updateProductIndex = products.findIndex((product)=> product._id===id);
+    if(updateProductIndex === -1){
+        throw new Error(`There is no product with this id: ${id}`);
+    }
+    products[updateProductIndex] = {...products[updateProductIndex], ...product};
+    return products[updateProductIndex];
+};
+
+const deleteProduct = (id)=>{
+    let updateProductIndex = products.findIndex((product)=> product._id===id);
+    if(updateProductIndex === -1){
+        throw new Error(`There is no product with this id: ${id}`);
+    }
+    products.splice(updateProductIndex,1);
+    return true;
 }
 
 module.exports = {
     getAllProducts,
     createNewProduct,
+    updateProduct,
+    deleteProduct,
 }
