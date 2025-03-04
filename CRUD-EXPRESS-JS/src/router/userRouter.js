42f9fa057b979609da60bca0d6898de9d8b73b43
Shawon <shawonh986@gmail.com>
2025-03-04 23:53:23 +0800

    formatting done

--- CRUD-EXPRESS-JS/src/router/userRouter.js
+++ CRUD-EXPRESS-JS/src/router/userRouter.js
@@ -1,33 +1,29 @@
-const express = require('express');
-const { userServices } = require('../services');
+const express = require("express");
+const { userServices } = require("../services");
 const userRouter = express.Router();
 
-
 //Get all users API
-userRouter.get('/', (req,res)=>{
-    res.send(userServices.getAllUsers());
+userRouter.get("/", (req, res) => {
+  res.send(userServices.getAllUsers());
 });
 
-
 //Create new user API
-userRouter.post('/', (req,res)=>{
-    res.status(201).json(userServices.createUser(req.body)); 
+userRouter.post("/", (req, res) => {
+  res.status(201).json(userServices.createUser(req.body));
 });
 
 //Update user API
-userRouter.put('/:id', (req,res)=>{
-    const { id } = req.params;
-    const updatedUser = userServices.updateUser(id,req.body);
-    res.status(201).json(updatedUser);
-
+userRouter.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedUser = userServices.updateUser(id, req.body);
+  res.status(201).json(updatedUser);
 });
 
 //Delete user API
-userRouter.delete('/:id', (req,res)=>{
-    const { id } = req.params;
-    userServices.deleteUser(id);
-    res.status(201).json({message:"User deleted successfully"});
+userRouter.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  userServices.deleteUser(id);
+  res.status(201).json({ message: "User deleted successfully" });
 });
-
 
 module.exports = userRouter;
