015d316fd89dec1682c2dddd9adec797270cb98a
Shawon <shawonh986@gmail.com>
2025-03-04 23:53:54 +0800

    formatting done for all file

--- CRUD-EXPRESS-JS/src/services/userServices.js
+++ CRUD-EXPRESS-JS/src/services/userServices.js
@@ -1,54 +1,52 @@
-const { v4: uuidv4 } = require('uuid');
-
+const { v4: uuidv4 } = require("uuid");
 
 //Users data
 const users = [
-    {
-        _id:"001",
-        name:"Shawon",
-        age :"24",
-        gender:"Male", 
-
-    },
-    {
-        _id:"002",
-        name:"Shozol",
-        age :"29",
-        gender:"Male",
-    }
+  {
+    _id: "001",
+    name: "Shawon",
+    age: "24",
+    gender: "Male",
+  },
+  {
+    _id: "002",
+    name: "Shozol",
+    age: "29",
+    gender: "Male",
+  },
 ];
 
-const getAllUsers = ()=> users;
+const getAllUsers = () => users;
 
-const createUser = (userData)=>{
-    const newUser = {_id: uuidv4(), ...userData}
-    users.push(newUser);
-    return newUser;
+const createUser = (userData) => {
+  const newUser = { _id: uuidv4(), ...userData };
+  users.push(newUser);
+  return newUser;
 };
 
-const updateUser = (id,user)=>{
-    let updatedUserIndex = users.findIndex((user) => user._id === id);
-    if(updatedUserIndex === -1){
-       throw new Error (`No user found with this id : ${id}`);
-    } 
-    
-    users[updatedUserIndex] = {...users[updatedUserIndex], ...user};
-    return users[updatedUserIndex];
+const updateUser = (id, user) => {
+  let updatedUserIndex = users.findIndex((user) => user._id === id);
+  if (updatedUserIndex === -1) {
+    throw new Error(`No user found with this id : ${id}`);
+  }
+
+  users[updatedUserIndex] = { ...users[updatedUserIndex], ...user };
+  return users[updatedUserIndex];
 };
 
-const deleteUser = (id)=>{
-    let userIndex = users.findIndex((user) => user._id === id);
-    if(userIndex === -1){
-        throw new Error (`No user found with this id : ${id}`);
-    }
+const deleteUser = (id) => {
+  let userIndex = users.findIndex((user) => user._id === id);
+  if (userIndex === -1) {
+    throw new Error(`No user found with this id : ${id}`);
+  }
 
-    users.splice(userIndex,1);
-    return true;
-}
+  users.splice(userIndex, 1);
+  return true;
+};
 
 module.exports = {
-    getAllUsers,
-    createUser,
-    updateUser,
-    deleteUser,
-}
+  getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+};
