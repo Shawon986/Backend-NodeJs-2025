4efca323dcf36e6cc42e9056a1f07e9e7125e890
Shawon <shawonh986@gmail.com>
2025-03-04 23:52:27 +0800

    fprmatted

--- CRUD-EXPRESS-JS/src/router/productRouter.js
+++ CRUD-EXPRESS-JS/src/router/productRouter.js
@@ -1,34 +1,30 @@
-const express = require('express');
-const { productServices } = require('../services');
-const { productMiddleware } = require('../middleware');
+const express = require("express");
+const { productServices } = require("../services");
+const { productMiddleware } = require("../middleware");
 const productRouter = express.Router();
 
-
 //get all products API
-productRouter.get('/',(req,res)=>{
-    res.send(productServices.getAllProducts());
+productRouter.get("/", (req, res) => {
+  res.send(productServices.getAllProducts());
 });
 
 //Create new product API
-productRouter.post('/',productMiddleware,(req,res)=>{
-    res.status(201).json(productServices.createNewProduct(req.body));
+productRouter.post("/", productMiddleware, (req, res) => {
+  res.status(201).json(productServices.createNewProduct(req.body));
 });
 
 //Update product API
-productRouter.put('/:id',(req,res)=>{
-    const {id} = req.params;
-    const updatedProduct = productServices.updateProduct(id,req.body);
-    res.status(201).json(updatedProduct);
+productRouter.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedProduct = productServices.updateProduct(id, req.body);
+  res.status(201).json(updatedProduct);
 });
 
 //Delete product API
-productRouter.delete('/:id',(req,res)=>{
-    const {id} = req.params;
-    productServices.deleteProduct(id);
-    res.json({message: "Product deleted successfully"})
+productRouter.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  productServices.deleteProduct(id);
+  res.json({ message: "Product deleted successfully" });
 });
-
-
-
 
 module.exports = productRouter;
