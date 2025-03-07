f4cc9b444e26114f34ffb6639896ce8fc6958638
Shawon <shawonh986@gmail.com>
2025-03-07 15:15:01 +0800

    mongoose, dotenv added and mongodb connection built

--- CRUD-EXPRESS-JS/src/app.js
+++ CRUD-EXPRESS-JS/src/app.js
@@ -1,10 +1,30 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
-const port = 5000;
+const mongoose = require('mongoose');
 const {configureRouter} = require('./router');
 const { logRequestMiddleware, errorHandler } = require('./middleware');
-app.use(express.json());
 
+
+const port = 5000;
+const app = express();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const MONGO_DB_NAME = process.env.MONGODB_NAME;
+
+const connectDB = ()=>{
+    console.log("Connecting MongoDB .....");
+    mongoose.connect(MONGODB_URI,{
+        dbName: MONGO_DB_NAME,
+    })
+    .then(()=>{
+        console.log("MONGODB connected successfully");
+    }).catch((err)=> console.err(`MONGODB connection failed !!! ${err}`));
+};
+
+connectDB();
+
+
+app.use(express.json());
 app.use(logRequestMiddleware);
 
 configureRouter(app);
