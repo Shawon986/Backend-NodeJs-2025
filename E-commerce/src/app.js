d235abccb60822449dced836f76c98206bf786d9
Shawon <shawonh986@gmail.com>
2025-03-10 02:47:19 +0800

    start new express js app project

---
+++ E-commerce/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const port = 9000 ;
+
+
+app.get('/test',(req,res)=>{
+    res.send("HI THERE !!!");
+})
+
+
+app.listen(port, ()=>{
+    console.log(`App is running on port ${port}`);
+})
