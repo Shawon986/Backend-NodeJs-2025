306490f0d02a221c0da78963829562647d1500de
Shawon <shawonh986@gmail.com>
2025-03-10 03:17:17 +0800

    get all products api done

--- E-commerce/src/app.js
+++ E-commerce/src/app.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const app = express();
-
 const port = 9000 ;
+app.use(express.json());
 
+const products = [
+    {
+        _id: 101,
+        model: "DJI MINI 2",
+        brand: "DJI",
+        categories: "Drone",
+        price: 2200,
+        
 
+    },
+    {
+        _id: 102,
+        model: "DJI AVATA 2",
+        brand: "DJI",
+        categories: "Drone",
+        price: 4200,
+    }
+]
+
+//Connection check API
 app.get('/test',(req,res)=>{
     res.send("HI THERE !!!");
 });
+
+//Get all products API
+app.get('/api/products',(req,res)=>{
+    res.send(products);
+});
+
 
 
 app.listen(port, ()=>{
