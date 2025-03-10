7847cba7f6262420dede8ff7288a0d9c6fd2562e
Shawon <shawonh986@gmail.com>
2025-03-10 15:02:55 +0800

    move router configuration away from app.js

--- E-commerce/src/app.js
+++ E-commerce/src/app.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const {productRouter} = require('./router');
+const configureRouter = require('./router');
 const app = express();
 const port = 9000 ;
 app.use(express.json());
 
-app.use('/api/products', productRouter);
 
-//Connection check API
-app.get('/test',(req,res)=>{
-    res.send("HI THERE !!!");
-});
-
+configureRouter(app);
 
 
 app.listen(port, ()=>{
--- E-commerce/src/router/index.js
+++ E-commerce/src/router/index.js
@@ -1,5 +1,13 @@
 const productRouter = require('./productRouter');
 
-module.exports = {
-    productRouter,
+const configureRouter = (app) => {
+    //Connection check API
+    app.get('/test',(req,res)=>{
+    res.send("HI THERE !!!");
+});
+
+app.use('/api/products', productRouter);
+
 };
+
+module.exports = configureRouter;
