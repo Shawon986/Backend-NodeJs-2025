3b0d5f6e7cc60c7b1b67a379a98c1a838295ee33
Shawon <shawonh986@gmail.com>
2025-03-10 14:28:08 +0800

    all api moved to router file

--- E-commerce/src/app.js
+++ E-commerce/src/app.js
@@ -1,79 +1,17 @@
 const express = require('express');
-const {v4: uuidv4} = require('uuid');
+const productRouter = require('./router/productRouter');
 const app = express();
 const port = 9000 ;
 app.use(express.json());
 
-const products = [
-    {
-        _id: uuidv4(),
-        model: "DJI MINI 2",
-        brand: "DJI",
-        categories: "Drone",
-        price: 2200,
-        
-
-    },
-    {
-        _id: uuidv4(),
-        model: "DJI AVATA 2",
-        brand: "DJI",
-        categories: "Drone",
-        price: 4200,
-    }
-]
+app.use(productRouter);
 
 //Connection check API
 app.get('/test',(req,res)=>{
     res.send("HI THERE !!!");
 });
 
-//Get all products API
-app.get('/api/products',(req,res)=>{
-    res.send(products);
-});
 
-//Get one product by id API
-app.get('/api/products/:id', (req,res)=>{
-    const id = req.params.id;
-    const product = products.find((product)=> product._id ===id);
-    if(!product){
-        res.send(`No product has found with this id ${id}`);
-    }
-    res.send(product);
-});
-
-//Create new product API
-app.post('/api/products', (req,res)=>{
-    let {payload} = req.body;
-    const newProduct = {_id: uuidv4(), ...payload};
-    products.push(newProduct);
-    res.status(201).json(newProduct);
-
-});
-
-//Update product API
-app.put('/api/products/:id', (req,res)=>{
-    const id = req.params.id;
-    const payload = req.body;
-    const productIndex = products.findIndex((product)=>product._id===id);
-    if(productIndex === -1){
-        return res.status(404).send(`No product has found with this id ${id}`);
-    }
-    products[productIndex] = { ...products[productIndex], ...payload};
-    res.json(products[productIndex]);
-});
-
-//Delete product api
-app.delete('/api/products/:id',(req,res)=>{
-    const id = req.params.id;
-    const productIndex = products.findIndex((product)=>product._id===id);
-    if(productIndex === -1){
-        return res.status(404).send(`No product has found with this id ${id}`);
-    }
-    products.splice(productIndex,1);
-    res.json({ message:"Product has deleted" });
-});
 
 app.listen(port, ()=>{
     console.log(`App is running on port ${port}`);
---
+++ E-commerce/src/router/productRouter.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const {v4: uuidv4} = require('uuid');
+
+
+const productRouter = express.Router();
+
+const products = [
+    {
+        _id: uuidv4(),
+        model: "DJI MINI 2",
+        brand: "DJI",
+        categories: "Drone",
+        price: 2200,
+        
+
+    },
+    {
+        _id: uuidv4(),
+        model: "DJI AVATA 2",
+        brand: "DJI",
+        categories: "Drone",
+        price: 4200,
+    }
+];
+
+//Get all products API
+productRouter.get('/api/products',(req,res)=>{
+    res.send(products);
+});
+
+//Get one product by id API
+productRouter.get('/api/products/:id', (req,res)=>{
+    const id = req.params.id;
+    const product = products.find((product)=> product._id ===id);
+    if(!product){
+        res.send(`No product has found with this id ${id}`);
+    }
+    res.send(product);
+});
+
+//Create new product API
+productRouter.post('/api/products', (req,res)=>{
+    let {payload} = req.body;
+    const newProduct = {_id: uuidv4(), ...payload};
+    products.push(newProduct);
+    res.status(201).json(newProduct);
+
+});
+
+//Update product API
+productRouter.put('/api/products/:id', (req,res)=>{
+    const id = req.params.id;
+    const payload = req.body;
+    const productIndex = products.findIndex((product)=>product._id===id);
+    if(productIndex === -1){
+        return res.status(404).send(`No product has found with this id ${id}`);
+    }
+    products[productIndex] = { ...products[productIndex], ...payload};
+    res.json(products[productIndex]);
+});
+
+//Delete product api
+productRouter.delete('/api/products/:id',(req,res)=>{
+    const id = req.params.id;
+    const productIndex = products.findIndex((product)=>product._id===id);
+    if(productIndex === -1){
+        return res.status(404).send(`No product has found with this id ${id}`);
+    }
+    products.splice(productIndex,1);
+    res.json({ message:"Product has deleted" });
+});
+
+module.exports = productRouter;
