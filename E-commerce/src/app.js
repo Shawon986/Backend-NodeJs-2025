3fc4d4d664575e25e7b4d9c98c2bf77bf105de8b
Shawon <shawonh986@gmail.com>
2025-03-12 20:41:30 +0800

    database connection done

--- E-commerce/src/app.js
+++ E-commerce/src/app.js
@@ -1,18 +1,38 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+
+
 const configureRouter = require('./router');
-
 const {logMiddleWare, errorHandler} = require('./Middleware');
-const app = express();
 
-app.use(express.json());
 
 const port = 9000 ;
 
+const app = express();
 
+const MONGO_URI = process.env.MONGO_URI;
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+
+
+
+const connectDB = ()=>{
+    console.log('Connecting to MongoDB');
+    mongoose.connect(MONGO_URI, {
+        dbName: MONGO_DB_NAME,
+    
+    })
+    .then(()=>{
+        console.log('Connected to MongoDB');
+    }).catch((err)=>{
+        console.error(`Error connecting to MongoDB ${err}`);
+    });
+};
+
+connectDB();
+
+app.use(express.json());
 app.use(logMiddleWare);
-
 
 configureRouter(app);
 
