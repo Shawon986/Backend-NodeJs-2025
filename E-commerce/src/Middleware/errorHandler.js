de2a3746eb5693bf8e2375840caeeba647fa5d79
Shawon <shawonh986@gmail.com>
2025-03-13 01:34:58 +0800

    add cast error handler

--- E-commerce/src/Middleware/errorHandler.js
+++ E-commerce/src/Middleware/errorHandler.js
@@ -5,7 +5,7 @@ const errorHandler = (err, req, res, next)=>{
     if (err instanceof notFoundError){
         return res.status(404).send(err.message);
     }
-    if(err instanceof mongoose.Error.ValidationError){
+    if(err instanceof mongoose.Error.ValidationError || err instanceof mongoose.Error.CastError){
         return res.status(404).send(err.message);
     }
     console.log(err.stack);
