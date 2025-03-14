52ac9be499fbfc8148fd40e0a46c1bc4748333b4
Shawon <shawonh986@gmail.com>
2025-03-14 16:20:17 +0800

    all product related api moved to product services

---
+++ E-commerce/src/services/productServices.js
@@ -0,0 +1,63 @@
+const {v4: uuidv4} = require('uuid');
+
+const products = [
+    {
+        _id: uuidv4(),
+        model: "DJI MINI 2",
+        brand: "DJI",
+        categories: "Drone",
+        price: 2200,
+        
+
+    },
+    {
+        _id: uuidv4(),
+        model: "DJI AVATA 2",
+        brand: "DJI",
+        categories: "Drone",
+        price: 4200,
+    }
+];
+
+const getAllProducts = () =>  products;
+
+const getProductById = (id) => {
+    const product = products.find((product) => product._id === id);
+    if(!product){
+        throw new Error(`No product has found with this id ${id}`);
+    }
+    return product;
+};
+
+const createProduct = (payload) => {
+    const newProduct = {_id: uuidv4(), ...payload};
+    products.push(newProduct);
+    return newProduct;
+};
+
+const updateProduct = (id, payload) => {
+    const productIndex = products.findIndex((product) => product._id === id);
+    if (productIndex === -1) {
+        throw new Error(`No product has found with this id ${id}`);
+    }
+    products[productIndex] = { ...products[productIndex], ...payload };
+    return products[productIndex];
+};
+
+const deleteProduct = (id) => {
+    const productIndex = products.findIndex((product) => product._id === id);
+    if (productIndex === -1) {
+        throw new Error(`No product has found with this id ${id}`);
+    }
+    products.splice(productIndex, 1);
+    return { message: "Product has deleted" };
+};
+
+module.exports = {
+    getAllProducts,
+    getProductById,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+};
+
