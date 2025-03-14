689796e913eca667bd00b8b3ec40b540a4d85091
Shawon <shawonh986@gmail.com>
2025-03-14 15:47:00 +0800

    app configure done

--- E-commerce/src/app.js
+++ E-commerce/src/app.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const { productRouter, userRouter } = require('./router');
+const { configureRouter } = require('./router');
 const app = express();
 const port = 9000 ;
 app.use(express.json());
 
 
 
-//Connection check API
-app.get('/test',(req,res)=>{
-    res.send("HI THERE !!!");
-});
-
-app.use('/api/products',productRouter);
-app.use('/api/users',userRouter);
+configureRouter(app);
 
 
 app.listen(port, ()=>{
--- E-commerce/src/router/index.js
+++ E-commerce/src/router/index.js
@@ -1,7 +1,16 @@
 const productRouter = require('./productRouter');
 const userRouter = require('./userRouter');
 
+const configureRouter = (app) => {
+  //Connection check API
+app.get('/test',(req,res)=>{
+    res.send("HI THERE !!!");
+});
+
+app.use('/api/products',productRouter);
+app.use('/api/users',userRouter);
+}
+
 module.exports = {
-  productRouter,
-    userRouter,
+    configureRouter,    
 };  
