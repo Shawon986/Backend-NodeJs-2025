3a60786824a9351fab0a3070db9f3779c8a0f7d7
Shawon <shawonh986@gmail.com>
2025-03-17 00:06:09 +0800

    database connection done

--- E-commerce/README.md
+++ E-commerce/README.md
@@ -4,3 +4,5 @@
 ### 2. npm install express --save
 ### 3. npm i -D nodemon  // its like live server to see update 
 ### 4. npm install uuid // to auto generate id
+### 5. npm install mongoose --save // to install mongoose for connecting mongodb
+### 6. npm i dotenv // to access env file
--- E-commerce/src/app.js
+++ E-commerce/src/app.js
@@ -1,12 +1,34 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const { configureRouter } = require('./router');
 const { logMiddleware } = require('./middleware');
 const { errorHandler } = require('./middleware/errorHandler');
 const app = express();
-const port = 9000 ;
+const MONGO_Db_URI = process.env.MONGODB_URI;
+const MONGO_DB_NAME = process.env.DB_NAME;
+
+
+
+const connectDb = ()=>{
+    console.log("Connecting to MongoDB");
+    mongoose.connect(MONGO_Db_URI,{
+        dbName: MONGO_DB_NAME,
+    }).then(()=>{
+        console.log("MongoDb Connected")
+    }).catch((error)=>{
+        console.error("Connection Failed")
+    });
+};
+
+connectDb();
+
+
+
 app.use(express.json());
 app.use(logMiddleware);
 
+const port =  process.env.PORT;
 
 configureRouter(app);
 
