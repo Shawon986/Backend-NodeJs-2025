8d5b5336c5f36b9666714619756363b41a2d69d6
Shawon <shawonh986@gmail.com>
2025-03-19 02:15:28 +0800

    add validation on update product also

--- E-commerce/src/router/productRouter.js
+++ E-commerce/src/router/productRouter.js
@@ -19,7 +19,7 @@ productRouter.get('/:id', productController.getProductById);
 productRouter.post('/', validatePayload(schemaOfProduct.omit({_id:true})), productController.createProduct);
 
 //Update product API
-productRouter.put('/:id', productController.updateProduct); 
+productRouter.put('/:id',validatePayload(schemaOfProduct), productController.updateProduct); 
 
 //Delete product api
 productRouter.delete('/:id', productController.deleteProduct);
